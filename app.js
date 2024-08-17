665f24a8f5c7246053f440ce594cadc4bb00eeba
Cristian Fuchslocher <fuchslocher.cristian@gmail.com>
2024-08-17 12:37:47 -0400

    funcion copiar y orden de variables

--- app.js
+++ app.js
@@ -1,34 +1,84 @@
 // Selección de los elementos del DOM
-const burro = document.querySelector(".encriptador__ingreso__texto__box__text");
+const inputText = document.querySelector(".encriptador__ingreso__texto__box__text");
 const textoResultado = document.querySelector(".box__text__encriptado_resuelto");
 const boxTextImg = document.getElementById("imagen_de_fondo");
 const boxTextText = document.querySelector(".box__resultado__textos");
-const botonCopiar = document.querySelector(".box__text__resuleto__Boton_copiar");
+const botonCopiar = document.querySelector(".box__text__resuelto__Boton_copiar");
+const boxResult = document.querySelector(".box__resultado");
+const boxTextEncrip = document.querySelector(".box__text_encriptado");
+const botonEncriptar = document.querySelector(".encriptador__ingreso__texto__Boton_encriptar");
+const botonDesencriptar = document.querySelector(".encriptador__ingreso__texto__Boton_desencriptar");
+
+// Asignación de eventos a botones
+botonEncriptar.addEventListener("click", btnEncriptar);
+botonDesencriptar.addEventListener("click", btnDesencriptar);
+botonCopiar.addEventListener("click", btnCopiar);
+
 
 // Función para encriptar texto
 function btnEncriptar() {
-    const textoEncriptar = encriptar(burro.value);
+    const textoEncriptar = encriptar(inputText.value);
     textoResultado.value = textoEncriptar;
-    burro.value = "";
-    boxTextImg.style.display = "none";  // Ocultar imagen de fondo
-    textoResultado.style.display = "block";  // Mostrar área de texto encriptado
-    boxTextText.style.display = "none";  // Ocultar texto de resultado
-    botonCopiar.style.display = "block";  // Mostrar botón de copiar
+    inputText.value = "";
+    mostrarResultado();
 }
 
 // Función que realiza los reemplazos de letras para encriptar el texto
 function encriptar(stringEncriptado) {
-    stringEncriptado = stringEncriptado.toLowerCase();
-    stringEncriptado = stringEncriptado.replaceAll("e", "enter")
-                                       .replaceAll("i", "imes")
-                                       .replaceAll("a", "ai")
-                                       .replaceAll("o", "ober")
-                                       .replaceAll("u", "ufat");
-    return stringEncriptado;
+    return stringEncriptado.toLowerCase()
+        
+        .replaceAll("e", "enter")
+        .replaceAll("i", "imes")
+        .replaceAll("a", "ai")
+        .replaceAll("o", "ober")
+        .replaceAll("u", "ufat");
+    
 }
 
-// Añadir el evento de clic al botón de encriptar
-document.querySelector(".encriptador__ingreso__texto__Boton_encriptar").addEventListener("click", btnEncriptar);
+// Función para desencriptar texto
+function btnDesencriptar() {
+    const textoDesencriptar = desencriptar(inputText.value);
+    textoResultado.value = textoDesencriptar;
+    inputText.value = "";
+    mostrarResultado();
+}
+
+// Función que realiza los reemplazos de letras para desencriptar el texto
+function desencriptar(stringDesencriptado) {
+    return stringDesencriptado.toLowerCase()
+        
+        .replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ai", "a")
+        .replaceAll("ober", "o")
+        .replaceAll("ufat", "u");
+}
+
+// Función para mostrar el resultado
+function mostrarResultado() {
+    boxTextImg.style.display = "none";      // Ocultar imagen de fondo
+    boxTextText.style.display = "none";     // Ocultar texto de resultado
+    boxResult.style.display = "none";       // Ocultar box resultado
+    boxTextEncrip.style.display = "flex";   // Mostrar box encriptado
+    textoResultado.style.display = "flex";  // Mostrar área de texto encriptado
+    botonCopiar.style.display = "flex";     // Mostrar botón de copiar
 
 
+}
 
+// Función para copiar el texto encriptado al portapapeles
+ function btnCopiar() {
+    const textoEncriptado = textoResultado.value;
+    navigator.clipboard.writeText(textoEncriptado)
+        .then(() => {
+            alert("Texto copiado al portapapeles");
+        })
+        .catch(err => {
+            console.error('Error al copiar el texto: ', err);
+        });
+}
+
+function validarTexto(texto) {
+    const regex = /^[a-z\s]*$/; // Solo letras minúsculas y espacios
+    return regex.test(texto);
+}
